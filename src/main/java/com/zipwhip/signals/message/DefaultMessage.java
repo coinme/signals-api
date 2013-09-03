3c7eb7c8b1eeed99adfdd60c2c41c6803097310e
Michael Smyers <msmyers@zipwhip.com>
2013-09-03 16:14:42 -0700

    Gson parsing now defaults to HashMap Now sharing DefaultMessage across projects rather than having each one create their own BasicMessage.

---
+++ src/main/java/com/zipwhip/signals/message/DefaultMessage.java
@@ -0,0 +1,73 @@
+package com.zipwhip.signals.message;
+
+import com.zipwhip.signals.address.Address;
+
+import java.io.Serializable;
+
+/**
+ * Date: 5/7/13
+ * Time: 6:13 PM
+ *
+ * @author Michael
+ * @version 1
+ */
+//@JsonAutoDetect(fieldVisibility=JsonAutoDetect.Visibility.PUBLIC_ONLY)
+public class DefaultMessage implements Message {
+
+    private static final long serialVersionUID = 8111225347753594028L;
+
+    private Address address;
+    private Serializable content;
+    private long timestamp;
+    private String id;
+    private String type;
+    private String event;
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Serializable getContent() {
+        return content;
+    }
+
+    public void setContent(Serializable content) {
+        this.content = content;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+}
