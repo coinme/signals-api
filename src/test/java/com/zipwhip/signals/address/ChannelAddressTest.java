cc82bf9d7d6a9db6ada058e519c96e4755d7bcf0
Russ Wirtz <rwirtz@zipwhip.com>
2013-09-03 12:47:49 -0700

    Add unit test

---
+++ src/test/java/com/zipwhip/signals/address/ChannelAddressTest.java
@@ -0,0 +1,39 @@
+package com.zipwhip.signals.address;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Russ
+ * Date: 9/3/13
+ * Time: 12:43 PM
+ */
+public class ChannelAddressTest {
+
+    private String channel1;
+    private ChannelAddress address1;
+
+    private String channel2;
+    private ChannelAddress address2;
+
+    @Before
+    public void setUp() throws Exception {
+        channel1 = UUID.randomUUID().toString();
+        channel2 = UUID.randomUUID().toString();
+
+        address1 = new ChannelAddress(channel1);
+        address2 = new ChannelAddress(channel2);
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+        assertEquals("Addresses weren't equal!", address1, address1);
+        assertTrue("Addresses weren't supposed to be equal!", !address1.equals(address2));
+    }
+}
