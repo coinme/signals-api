bbdf1d4c38728b68e35f423fadd32520fff67434
Russ Wirtz <rwirtz@zipwhip.com>
2013-09-10 15:34:46 -0700

    SignalMessage implements Serializable

--- src/main/java/com/zipwhip/signals2/SignalMessage.java
+++ src/main/java/com/zipwhip/signals2/SignalMessage.java
@@ -1,12 +1,16 @@
 package com.zipwhip.signals2;
 
+import java.io.Serializable;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Russ
  * Date: 9/10/13
  * Time: 3:03 PM
  */
-public class SignalMessage {
+public class SignalMessage implements Serializable {
+
+    private static final long serialVersionUID = -5631019663174617721L;
 
     private String address;
     private String body;
@@ -133,5 +137,53 @@ public class SignalMessage {
 
     public void setHasAttachments(boolean hasAttachments) {
         this.hasAttachments = hasAttachments;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SignalMessage that = (SignalMessage) o;
+
+        if (hasAttachments != that.hasAttachments) return false;
+        if (address != null ? !address.equals(that.address) : that.address != null) return false;
+        if (advertisement != null ? !advertisement.equals(that.advertisement) : that.advertisement != null)
+            return false;
+        if (body != null ? !body.equals(that.body) : that.body != null) return false;
+        if (contactDeviceId != null ? !contactDeviceId.equals(that.contactDeviceId) : that.contactDeviceId != null)
+            return false;
+        if (contactId != null ? !contactId.equals(that.contactId) : that.contactId != null) return false;
+        if (dateCreated != null ? !dateCreated.equals(that.dateCreated) : that.dateCreated != null) return false;
+        if (dateRead != null ? !dateRead.equals(that.dateRead) : that.dateRead != null) return false;
+        if (deviceId != null ? !deviceId.equals(that.deviceId) : that.deviceId != null) return false;
+        if (fingerprint != null ? !fingerprint.equals(that.fingerprint) : that.fingerprint != null) return false;
+        if (fromName != null ? !fromName.equals(that.fromName) : that.fromName != null) return false;
+        if (messageType != null ? !messageType.equals(that.messageType) : that.messageType != null) return false;
+        if (scheduledDate != null ? !scheduledDate.equals(that.scheduledDate) : that.scheduledDate != null)
+            return false;
+        if (transmissionState != null ? !transmissionState.equals(that.transmissionState) : that.transmissionState != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = address != null ? address.hashCode() : 0;
+        result = 31 * result + (body != null ? body.hashCode() : 0);
+        result = 31 * result + (transmissionState != null ? transmissionState.hashCode() : 0);
+        result = 31 * result + (advertisement != null ? advertisement.hashCode() : 0);
+        result = 31 * result + (fromName != null ? fromName.hashCode() : 0);
+        result = 31 * result + (fingerprint != null ? fingerprint.hashCode() : 0);
+        result = 31 * result + (deviceId != null ? deviceId.hashCode() : 0);
+        result = 31 * result + (contactId != null ? contactId.hashCode() : 0);
+        result = 31 * result + (contactDeviceId != null ? contactDeviceId.hashCode() : 0);
+        result = 31 * result + (messageType != null ? messageType.hashCode() : 0);
+        result = 31 * result + (scheduledDate != null ? scheduledDate.hashCode() : 0);
+        result = 31 * result + (dateRead != null ? dateRead.hashCode() : 0);
+        result = 31 * result + (dateCreated != null ? dateCreated.hashCode() : 0);
+        result = 31 * result + (hasAttachments ? 1 : 0);
+        return result;
     }
 }
