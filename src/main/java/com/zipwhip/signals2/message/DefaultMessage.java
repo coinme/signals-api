6bc5f8d3936fca961e62b93234db58fdab59ad39
Michael Smyers <msmyers@zipwhip.com>
2013-09-20 17:40:08 -0700

    equals/hashcode

--- src/main/java/com/zipwhip/signals2/message/DefaultMessage.java
+++ src/main/java/com/zipwhip/signals2/message/DefaultMessage.java
@@ -71,6 +71,34 @@ public class DefaultMessage implements Message {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof DefaultMessage)) return false;
+
+        DefaultMessage message = (DefaultMessage) o;
+
+        if (timestamp != message.timestamp) return false;
+        if (address != null ? !address.equals(message.address) : message.address != null) return false;
+        if (content != null ? !content.equals(message.content) : message.content != null) return false;
+        if (event != null ? !event.equals(message.event) : message.event != null) return false;
+        if (id != null ? !id.equals(message.id) : message.id != null) return false;
+        if (type != null ? !type.equals(message.type) : message.type != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = address != null ? address.hashCode() : 0;
+        result = 31 * result + (content != null ? content.hashCode() : 0);
+        result = 31 * result + (int) (timestamp ^ (timestamp >>> 32));
+        result = 31 * result + (id != null ? id.hashCode() : 0);
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        result = 31 * result + (event != null ? event.hashCode() : 0);
+        return result;
+    }
+
+    @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder();
         sb.append("DefaultMessage");
