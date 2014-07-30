2a247c0e79afa3741f2db386e04addb099b4fcfb
Michael Smyers <msmyers@zipwhip.com>
2014-07-29 18:20:22 -0700

    New address type for singleton Void style objects.

---
+++ src/main/java/com/zipwhip/signals2/address/VoidAddress.java
@@ -0,0 +1,29 @@
+package com.zipwhip.signals2.address;
+
+/**
+ * @author Michael
+ * @date 7/29/2014
+ */
+public class VoidAddress extends AddressBase {
+
+    private static final long serialVersionUID = 2713878108203598196L;
+
+    private static final VoidAddress instance = new VoidAddress();
+
+    private VoidAddress() {
+
+    }
+
+    @Override
+    public int compareTo(Address o) {
+        if (o instanceof VoidAddress) {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    public static VoidAddress getInstance() {
+        return instance;
+    }
+}
